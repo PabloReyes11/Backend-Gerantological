82fafac1458fa0153133bf397383fd9fd7d5aff7
ReplacedSpace17 <replacedspace17@gmail.com>
2024-05-10 19:52:35 -0600

    feat: Update CORS configuration in Express app to allow specific origin, methods, and headers

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const multer = require('multer'); // Middleware para manejar archivos en formula
 
 // Configuración del app
 
-// Middleware para establecer las cabeceras de seguridad
+app.use(cors({
+  origin: 'https://www.google.com', // Reemplaza con el origen específico que deseas permitir
+  methods: ['GET', 'POST'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+}));
 app.use((req, res, next) => {
   res.setHeader('Content-Security-Policy', "default-src 'self' https://www.google.com"); // Permite recursos desde el mismo origen y desde google.com
   next();
